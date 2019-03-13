cbd4f667d06ecd49423f8e49e99e02ea6a3b346c
Ashwin Vijendra <ack4and@gmail.com>
2019-03-13 16:33:15 +0530

    added check for entered letters exists in original

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -7,14 +7,20 @@ class GamesController < ApplicationController
 
   def score
     entered_word = params[:answer]
-    # use API to check conditions
-    url = "https://wagon-dictionary.herokuapp.com/#{entered_word}"
-    word_serialized = open(url).read
-    @result = JSON.parse(word_serialized)
-    @found = if @result['found']
-               'valid'
-             else
-               'invalid'
-             end
+    entered_letters_array = params[:answer].downcase.chars
+    original_letters_array = params[:letters].delete(' ').downcase.chars
+
+    if (entered_letters_array - original_letters_array).empty?
+      url = "https://wagon-dictionary.herokuapp.com/#{entered_word}"
+      word_serialized = open(url).read
+      @result = JSON.parse(word_serialized)
+      @found = if @result['found']
+                 "Congratulations!! #{entered_word} is a valid English word."
+               else
+                 "Sorry but #{entered_word} doesn't seem to be a valid English word"
+               end
+    else
+      @cant_be_built = "Sorry but #{entered_word} can't be built out of the letters given"
+    end
   end
 end
